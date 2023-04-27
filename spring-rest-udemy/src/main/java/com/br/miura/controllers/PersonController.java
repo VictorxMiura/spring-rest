8b5250c1cca275ccf8757ea18b640b4abd2e9d00
Victor <miura.hublab@gmail.com>
2023-04-27 20:26:04 -0300

    Add support to return XML/YAML responses

--- spring-rest-udemy/src/main/java/com/br/miura/controllers/PersonController.java
+++ spring-rest-udemy/src/main/java/com/br/miura/controllers/PersonController.java
@@ -3,13 +3,14 @@ package com.br.miura.controllers;
 import com.br.miura.data.vo.v1.PersonVO;
 import com.br.miura.data.vo.v2.PersonVOV2;
 import com.br.miura.services.PersonServices;
+
+import com.br.miura.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
+
+
 
 
 @RestController
@@ -20,31 +21,41 @@ public class PersonController {
     private PersonServices services;
 
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonVO findById (@PathVariable (value = "id") Long id)
         {
         return services.findById(id);
     }
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+
+
+    @GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML })
     public List<PersonVO> findAll (){
 
         return services.findAll();
     }
 
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML },
+            consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
     public PersonVO create(@RequestBody PersonVO person){
         return services.create(person);
     }
 
-//    @PostMapping(value = "/v2", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+
+
+//    @PostMapping(value = "/v2", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 //    public PersonVOV2 createV2(@RequestBody PersonVOV2 person){
 //        return services.createV2(person);
 //    }
 
-    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+
+
+    @PutMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML},
+            consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
     public PersonVO update(@RequestBody PersonVO person){
         return services.update(person);
     }
+
+
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
--- spring-rest-udemy/src/main/resources/db/migration/V1__Create_Table_Person.sql
+++ spring-rest-udemy/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -1,3 +1,7 @@
+
+
+
+
 CREATE TABLE public.person (
     id bigint NOT NULL,
     address character varying(100) NOT NULL,
@@ -5,3 +9,33 @@ CREATE TABLE public.person (
     gender character varying(6) NOT NULL,
     last_name character varying(80) NOT NULL
 );
+
+
+
+CREATE SEQUENCE public.person_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+ALTER SEQUENCE public.person_id_seq OWNED BY public.person.id;
+
+
+
+
+ALTER TABLE ONLY public.person ALTER COLUMN id SET DEFAULT nextval('public.person_id_seq'::regclass);
+
+
+
+
+SELECT pg_catalog.setval('public.person_id_seq', 14, true);
+
+
+
+ALTER TABLE ONLY public.person
+    ADD CONSTRAINT person_pkey PRIMARY KEY (id);
+
+
